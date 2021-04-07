534b722d2c0cf3d6860e2b77b8ae338ffd2bf1ed
eversnow-lilette <74845586+eversnow-lilette@users.noreply.github.com>
2021-04-07 10:37:13 +0900

    comments

--- assets/application.js
+++ assets/application.js
@@ -1,9 +1,11 @@
 // Put your application javascript here
+
 //Cart Item Counter
 function addCartIndex(){
     let addCartForm = document.querySelector('form[action="/cart/add"]');
     let formData = new FormData(addCartForm);
 
+    //editting the cart index line property 
     formData.append("cart_index", document.getElementById('item_index').value);
 
     fetch('/cart/add.js', {
@@ -18,12 +20,14 @@ function addCartIndex(){
       });
 }
 
+//counts the items in the cart
 function addIndex(){
     var count = parseInt(document.getElementById('item_index').value) + 0;
     count ++;
     document.getElementById('item_index').setAttribute('value', count);
 }
 
+//counts the quantities of the items in the cart
 function addItems(item, quantity, amount){
    var items = "Item: " + item + " Quantity: " + quantity + " Amount: " + amount +"\n";
    var value = document.getElementById('item_list').value
@@ -33,6 +37,7 @@ function addItems(item, quantity, amount){
    document.getElementById('item_list').setAttribute('value', value)
 }
 
+//sets the modal message
 function openModal(stock){ 
 
   var message = "";
@@ -43,6 +48,7 @@ function openModal(stock){
     alert(message);
 }
 
+//prints the text in the console
 function printConsole(text){
     console.log(text);
 }
